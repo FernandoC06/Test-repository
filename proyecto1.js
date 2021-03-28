de197ef6abdbb94450181f826e3ad63297afd317
Fernando Castro <fernandocastro@MacBook-Air-de-Fernando.local>
2021-03-27 22:34:26 -0700

    This is just a test for my first repository

---
+++ proyecto1.js
@@ -0,0 +1,50 @@
+//Práctica características de persona
+const nombre = "Carlos Fernando";
+const apellidoPaterno = "Castro";
+const apellidoMaterno = "Ochoa";
+var edad = 18;
+const ciudadNacimiento = "Hermosillo";
+
+console.log( nombre );
+
+console.log( edad );
+
+console.log( ciudadNacimiento );
+
+//Datos característicos de un carro
+const marca = "Acura";
+const anio = 2007;
+const modelo = "TL";
+const país = "Japón";
+var color = "azul";
+
+//Tipo de dato Objeto
+const carroObjeto = {
+    marcaObjeto: "Nissan",
+    anioObjeto: 2008,
+    modeloObjeto: "Platina",
+    paisObjeto:"Japon",
+    colorObjeto: "Azul"
+}
+
+console.log ( carroObjeto )
+
+//Tipo de dato Undefined, usado para cuando aún no sabemos cuál será el dato
+var datoIndefinido;
+
+console.log ( datoIndefinido )
+
+//Tipo de dato null, se reserva un espacio vacío sin dato en sí 
+const datoNulo = null;
+
+console.log ( datoNulo )
+
+//Tipo de dato Boolean
+var datoBooleano = true;
+var datoBooleano2 = false; 
+
+//Tipo de dato Arreglo, es un conjunto ordenado de valores 
+const arregloNumerico = [ 10, 20, 30, 40, 50, 60 ];
+const arregloPalabra = [ "María, Carlos, Ernesto, Alia" ];
+const arregloMixto = [ 10, 15, "Diego", 20, "Alicia", 50, "Dylan" ];
+
